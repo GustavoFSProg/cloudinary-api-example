71a1c980e142e4ae0dd9b65384ff2d08f652eab1
Gustavo <gustavo.prog40@gmail.com>
2022-07-07 09:09:07 -0300

    fazendo

--- api/src/config/uploadConfig.ts
+++ api/src/config/uploadConfig.ts
@@ -1,10 +1,9 @@
 import multer from 'multer'
-import path from 'path'
 
 const uploadConfig = {
   // eslint-disable-next-line new-cap
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    // destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename(_req, file, cb) {
       const [name] = file.originalname.split('.')
       // const filename = `${name}.mp4`
